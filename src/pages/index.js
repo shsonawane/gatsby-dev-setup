b1671d998d81a4066ecab8e519d7594e7994a531
Shubham Sonawane <40923652+shsonawane@users.noreply.github.com>
2021-12-19 13:53:47 -0500

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const Index = () => (
+  <Layout>
+    <div>
+      <h1>Home Page</h1>
+      <p>You made it!</p>
+    </div>
+  </Layout>
+)
+
+export default Index
